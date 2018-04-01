0a53ec07935ab85a363cabc49db45f0edcbf2453
wangfeng <will.wangfeng@gmail.com>
2018-04-02 07:53:07 +0800

    需求：根据用户名称模糊查询用户信息

--- src/cn/itcast/first/MyBatisFirst.java
+++ src/cn/itcast/first/MyBatisFirst.java
@@ -2,6 +2,7 @@ package cn.itcast.first;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -45,4 +46,27 @@ public class MyBatisFirst {
 		//释放资源
 		sqlSession.close();
 	}
+	
+	//根据用户名称模糊查询用户信息
+		@Test
+		public void findUserByNameTest() throws IOException{
+			
+			//mybatis配置文件
+			String resource = "SqlMapConfig.xml";
+			
+			//得到配置文件流
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			//创建会话工厂，传入mybatis的配置信息
+			
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			
+			//通过工厂得到sqlSession
+			SqlSession sqlSession = sqlSessionFactory.openSession();
+			
+			List<User> list = sqlSession.selectList("test.findUserByName", "小明");
+			
+			System.out.println(list);
+			//释放资源
+			sqlSession.close();
+		}
 }
